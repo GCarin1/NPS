29248a5531b9c4c6aca2f031403d469e6ce42e6b
Guilherme Carini <carinigcontact@gmail.com>
2021-04-10 17:10:07 -0300

    .

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,34 +1,58 @@
-import { Request, Response } from "express";
-import { getCustomRepository } from "typeorm";
-import { User } from "../models/User";
-import { UserRepository } from "../repositories/UsersRepository";
+import { Request, Response } from 'express'; // Express para as requisições e respostas
+import { getCustomRepository, UpdateDateColumn } from 'typeorm';
+import { UsersRepository } from '../repositories/UsersRepository'; // Aqui onde está os comandos de acesso o banco de dados por meio do TypeORM de User
+import * as yup from 'yup';
+import { AppError } from '../errors/AppError';
+class UserController {
 
-class UserController{
-    async create(request: Request,response:Response){
-        const {name,email}=request.body;
+    async create(request: Request, response: Response) {
+        // Desestruturação pois sabe que aqui está recebendo apenas o nome e email do JSON requisitado
+       const { name, email } = request.body;
 
-        const userRepository= getCustomRepository(UserRepository);
-
-        const userAlreadyExists= await userRepository.findOne({
-            email
-        });
-        if(userAlreadyExists){
-            return response.status(400).json({
-                error:"User already exists!",
-            });
-        }
-
-
-        const user = userRepository.create({
-            name,email
-
+       // Obrigatório apra cadastrar usuário
+        const scheme = yup.object().shape({
+            name: yup.string().required("Nome é obrigatório!"),
+            email: yup.string().email("Email inválido!").required("Email é obrigatório")
         })
-        await userRepository.save(user);
 
+        // Primeira forma de validar:
+        // Valindo o objeto, se não for válido:
+        //    if( !(await scheme.isValid(request.body)) ) {
+        //        return response.status(400).json (
+        //            {error: "Validation Failed!"}
+        //        );
+        //    }
 
-        return response.status(201).json(user);
-     
+        // Segunda forma de validar
+        // Com try catch tratando:
+        try {
+            await scheme.validate(request.body, {abortEarly: false}); // AbortEarly para fazer todas as validações
+        } catch (err) {
+            throw new AppError(err);
+        }
+      
+       const usersRepository = getCustomRepository(UsersRepository);
+
+       const userAlreadyExists = await usersRepository.findOne({
+           email
+       });
+
+      
+       if(userAlreadyExists) {
+        throw new AppError("User already exists!");
+       }
+
+       
+       const user = usersRepository.create({
+            name, email
+       })
+
+       
+       await usersRepository.save(user);
+
+       return response.status(201).json(user);
     }
+
 }
 
 export { UserController };
